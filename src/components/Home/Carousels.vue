ffed55954ce95c8cf58b0405b8120cbff1975d53
El-Litios <carlosguzmanrojas00@gmail.com>
2023-05-23 22:50:10 -0400

    componente Carousel

---
+++ src/components/Home/Carousels.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-carousel hide-delimiters :show-arrows="false">
+  <v-carousel-item
+    src="src/assets/images/carousel-image.jpg"
+    cover
+  ></v-carousel-item>
+</v-carousel>
+</template>
+
+<script>
+export default {
+  setup() {
+    
+  },
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
