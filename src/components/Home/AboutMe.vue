3cb5afdf11aa0a909c188ba3c78d499b79d429ec
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-11 16:45:20 -0400

    Componente Acerca de mi

---
+++ src/components/Home/AboutMe.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <div class="title mt-4">
+        <h1>Acerca de mi:</h1>
+    </div>
+
+    <div class="content">
+
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css" scoped>
+.title{
+    text-align: center;
+    background-color: #5455aa;
+    margin-right: 40%;
+    margin-left: 40%;
+    color: #f5f5dc;
+}
+
+@media (max-width: 1000px) {
+  .title{
+    margin-right: auto;
+    margin-left: auto;
+}
+}
+</style>
