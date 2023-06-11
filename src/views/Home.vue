ce33e5d9e3ec4649bca7684889402b91c4079485
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-11 16:45:48 -0400

    Importacion de AboutMe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <Navbar/>
   <Carousels/>
+  <AboutMe/>
 </template>
 
 <script>
 import Navbar from '../components/shared/Navbar.vue'
 import Carousels from '../components/Home/Carousels.vue'
+import AboutMe from '../components/Home/AboutMe.vue'
 export default {
-  components:{ Navbar, Carousels}
+  components:{ Navbar, Carousels, AboutMe}
 }
 </script>
 
