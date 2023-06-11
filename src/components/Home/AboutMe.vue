7f541a4f5371171aa91f7f8c1ac16b6e7e15fad3
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-11 18:52:23 -0400

    Agrega descripción

--- src/components/Home/AboutMe.vue
+++ src/components/Home/AboutMe.vue
@@ -1,34 +1,60 @@
 <template>
   <v-container>
     <div class="title mt-4">
-        <h1>Acerca de mi:</h1>
+      <h1>Acerca de mi:</h1>
     </div>
 
-    <div class="content">
-
+    <div class="content mt-3">
+      <p class="aboutme">
+        ¡Hola!, soy Ingeniero en Informática apasionado por la programación y el
+        desarrollo de software tanto FrontEnd como BackEnd. Estoy dispuesto a
+        tomar retos desafiantes para utilizar todas mis capacidades como
+        desarrollador y lograr objetivos. Además, siempre estoy motivado a
+        aprender cosas nuevas que me aporten a mi crecimiento profesional.
+      </p>
     </div>
   </v-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="css" scoped>
-.title{
-    text-align: center;
-    background-color: #5455aa;
-    margin-right: 40%;
-    margin-left: 40%;
-    color: #f5f5dc;
+.title {
+  text-align: center;
+  background-color: rgba(84, 85, 170, 0.4);
+  margin-right: 40%;
+  margin-left: 40%;
+  color: #f5f5dc;
+  border-radius: 5%;
+}
+
+.content {
+  text-align: justify;
+  background-color: rgba(84, 85, 170, 0.4);
+  margin-right: 20%;
+  margin-left: 20%;
+  border-radius: 10px;
+  padding: 10px 10px 10px 10px;
+}
+
+.aboutme {
+  color: #f5f5dc;
+  font-size: 20px;
 }
 
 @media (max-width: 1000px) {
-  .title{
+  .title {
     margin-right: auto;
     margin-left: auto;
-}
+  }
+  .content {
+    margin-right: auto;
+    margin-left: auto;
+  }
+  .aboutme {
+    font-size: 15px;
+  }
 }
 </style>
