27d051a0754111aa879782d0bd224a81571b602b
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-13 23:34:57 -0400

    Importacion de Projects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,16 @@
   <Navbar/>
   <Carousels/>
   <AboutMe/>
+  <Projects/>
 </template>
 
 <script>
 import Navbar from '../components/shared/Navbar.vue'
 import Carousels from '../components/Home/Carousels.vue'
 import AboutMe from '../components/Home/AboutMe.vue'
+import Projects from '../components/Home/Projects.vue'
 export default {
-  components:{ Navbar, Carousels, AboutMe}
+  components:{ Navbar, Carousels, AboutMe, Projects}
 }
 </script>
 
