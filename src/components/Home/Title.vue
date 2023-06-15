1fdd72f1318256311f25022425086b0e731efcc2
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-14 20:30:14 -0400

    componente para titulo con slots

---
+++ src/components/Home/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="title mt-4">
+      <slot name="title"></slot>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.title {
+  text-align: center;
+  background-color: rgba(84, 85, 170, 0.4);
+  margin-right: 40%;
+  margin-left: 40%;
+  color: #f5f5dc;
+  border-radius: 5%;
+}
+</style>
