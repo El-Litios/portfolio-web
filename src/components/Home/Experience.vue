e2534d8b66449534a858affb184b4665ac376409
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-16 00:06:43 -0400

    Lista de experiencia laboral

--- src/components/Home/Experience.vue
+++ src/components/Home/Experience.vue
@@ -1,13 +1,46 @@
 <template>
-  <Title>
-      <template v-slot:title><h1>Proyectos</h1></template>
+  <v-container >
+    <Title>
+    <template v-slot:title><h1>Experiencia</h1></template>
     </Title>
+    <v-row class="mt-3">
+      <v-col cols="12">
+        <v-row v-for="e in experiences"  :key="e.id">
+          <v-col cols="4">
+            <v-img width="70" height="70" :src="e.srcIcon"></v-img>
+          </v-col>
+          <v-divider color="white" vertical></v-divider>
+          <v-col>
+            <h3>{{ e.companyName }}, {{ e.typeContract }}</h3>
+            <v-divider color="white" :thickness="2"></v-divider>
+            <h4>{{ e.location }}, {{ e.type }}</h4>
+            <h5>{{ e.initDate }} - {{ e.endDate }}</h5>
+            <p><b>Descipción: </b> {{ e.description }}</p>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-import Title from '@/components/Home/Ttle.vue'
+import { ref } from "vue";
+import experience from "@/data/experience.json";
+import Title from '@/components/Home/Title.vue'
+const experiences = ref(experience);
 </script>
 
-<style>
+<style lang="css" scoped>
+.v-row {
+  background-color: rgba(84, 85, 170, 0.4);
+  color: rgba(245, 245, 220, 0.9);
+}
 
+.v-img{
+  margin: auto;
+}
+
+.v-col h4{
+  font-weight: 300;
+}
 </style>
