fe92427f9bcedbf75f07b52becb5b83a201146ed
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-16 23:48:08 -0400

    Footer

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer class="text-center d-flex flex-column mt-5">
+
+    <div>{{ new Date().getFullYear() }} — <strong>Desarrollado por: </strong>Carlos Guzmán Rojas</div>
+  </v-footer>
+</template>
+
+<script setup>
+</script>
+
+<style lang="css" scoped>
+.v-footer{
+    background-color: rgba(84, 85, 170, 0.6);
+    color: #f5f5dc;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,18 @@
   <AboutMe/>
   <Projects/>
   <Experience/>
+  <Footer />
 </template>
 
 <script>
 import Navbar from '../components/shared/Navbar.vue'
+import Footer from '../components/shared/Footer.vue'
 import Carousels from '../components/Home/Carousels.vue'
 import AboutMe from '../components/Home/AboutMe.vue'
 import Projects from '../components/Home/Projects.vue'
 import Experience from '../components/Home/Experience.vue'
 export default {
-  components:{ Navbar, Carousels, AboutMe, Projects, Experience}
+  components:{ Navbar, Carousels, AboutMe, Projects, Experience, Footer}
 }
 </script>
 
