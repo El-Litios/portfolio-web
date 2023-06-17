4b5fb96719e210ebcf532843003e62b089d18bf4
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-16 23:12:12 -0400

    Medios de contacto

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app-bar :elevation="0">
     <template v-slot:append>
-      <v-btn icon="mdi-github" size="large"></v-btn>
-
-      <v-btn icon="mdi-linkedin" size="large"></v-btn>
+      <v-btn icon="mdi-github" href="https://github.com/El-Litios" size="large"></v-btn>
+      <v-btn icon="mdi-linkedin" href="https://www.linkedin.com/in/cguzmanr/" size="large"></v-btn>
+      <v-btn icon="mdi-email" href="mailto:carlosguzmanrojas00@gmail.com" size="large"></v-btn>
 
     </template>
   </v-app-bar>
