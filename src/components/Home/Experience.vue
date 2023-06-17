8ab7e96dcc41d1dac6cfec5e9f203658367957fc
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-16 23:11:21 -0400

    Agrega box shadow

--- src/components/Home/Experience.vue
+++ src/components/Home/Experience.vue
@@ -3,7 +3,7 @@
     <Title>
     <template v-slot:title><h1>Experiencia</h1></template>
     </Title>
-    <v-row class="mt-3">
+    <v-row class="mt-3 experience-box">
       <v-col cols="12">
         <v-row v-for="e in experiences"  :key="e.id">
           <v-col cols="4">
@@ -32,10 +32,14 @@ const experiences = ref(experience);
 
 <style lang="css" scoped>
 .v-row {
-  background-color: rgba(84, 85, 170, 0.4);
+  background-color: rgba(84, 85, 170, 0.6);
   color: rgba(245, 245, 220, 0.9);
+  
 }
 
+.experience-box{
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.55);
+}
 .v-img{
   margin: auto;
 }
