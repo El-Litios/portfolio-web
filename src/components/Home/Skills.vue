107eb58b43da8ec19321bd7d687008a5edc1b412
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-16 23:11:54 -0400

    Import de componente title

---
+++ src/components/Home/Skills.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <Title>
+    <template v-slot:title><h1>Habilidades</h1></template>
+    </Title>
+    
+  </v-container>
+</template>
+
+<script setup>
+import Title from "@/components/Home/Title.vue"
+
+</script>
+
+<style>
+
+</style>
